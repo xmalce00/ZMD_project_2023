46e7bfcea1d28717897b35e047c451c8aa3f056d
xmalce00 <60735480+xmalce00@users.noreply.github.com>
2023-04-01 12:58:16 +0200

    Update

--- JPEG_coder/src/main/java/Jpeg/Process.java
+++ JPEG_coder/src/main/java/Jpeg/Process.java
@@ -5,9 +5,9 @@ import Enums.SamplingTypes;
 import Enums.TransformTypes;
 import Enums.YcRcBTypes;
 import Jama.Matrix;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+//import org.apache.poi.hssf.usermodel.HSSFRow;
+//import org.apache.poi.hssf.usermodel.HSSFSheet;
+//import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -475,43 +475,43 @@ public class Process {
         modifiedCb = Quantization.inverseQuantize(modifiedCb, blockSize, quality, false);
     }
 
-    public void generateExcelFile() {
-        try
-        {
-            String filename = "Balance.xls";
-            HSSFWorkbook workbook = new HSSFWorkbook();
-            HSSFSheet sheet = workbook.createSheet("January");
-
-            HSSFRow rowhead = sheet.createRow((short)0);
-            rowhead.createCell(0).setCellValue("S.No.");
-            rowhead.createCell(1).setCellValue("Customer Name");
-            rowhead.createCell(2).setCellValue("Account Number");
-            rowhead.createCell(3).setCellValue("e-mail");
-            rowhead.createCell(4).setCellValue("Balance");
-
-            HSSFRow row = sheet.createRow((short)1);
-            row.createCell(0).setCellValue("1");
-            row.createCell(1).setCellValue("John William");
-            row.createCell(2).setCellValue("9999999");
-            row.createCell(3).setCellValue("william.john@gmail.com");
-            row.createCell(4).setCellValue("700000.00");
-
-            HSSFRow row1 = sheet.createRow((short)2);
-            row1.createCell(0).setCellValue("2");
-            row1.createCell(1).setCellValue("Mathew Parker");
-            row1.createCell(2).setCellValue("22222222");
-            row1.createCell(3).setCellValue("parker.mathew@gmail.com");
-            row1.createCell(4).setCellValue("200000.00");
-
-            FileOutputStream fileOut = new FileOutputStream(filename);
-            workbook.write(fileOut);
-            fileOut.close();
-            workbook.close();
-            System.out.println("Excel file has been generated successfully.");
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
+//    public void generateExcelFile() {
+//        try
+//        {
+//            String filename = "Balance.xls";
+//            HSSFWorkbook workbook = new HSSFWorkbook();
+//            HSSFSheet sheet = workbook.createSheet("January");
+//
+//            HSSFRow rowhead = sheet.createRow((short)0);
+//            rowhead.createCell(0).setCellValue("S.No.");
+//            rowhead.createCell(1).setCellValue("Customer Name");
+//            rowhead.createCell(2).setCellValue("Account Number");
+//            rowhead.createCell(3).setCellValue("e-mail");
+//            rowhead.createCell(4).setCellValue("Balance");
+//
+//            HSSFRow row = sheet.createRow((short)1);
+//            row.createCell(0).setCellValue("1");
+//            row.createCell(1).setCellValue("John William");
+//            row.createCell(2).setCellValue("9999999");
+//            row.createCell(3).setCellValue("william.john@gmail.com");
+//            row.createCell(4).setCellValue("700000.00");
+//
+//            HSSFRow row1 = sheet.createRow((short)2);
+//            row1.createCell(0).setCellValue("2");
+//            row1.createCell(1).setCellValue("Mathew Parker");
+//            row1.createCell(2).setCellValue("22222222");
+//            row1.createCell(3).setCellValue("parker.mathew@gmail.com");
+//            row1.createCell(4).setCellValue("200000.00");
+//
+//            FileOutputStream fileOut = new FileOutputStream(filename);
+//            workbook.write(fileOut);
+//            fileOut.close();
+//            workbook.close();
+//            System.out.println("Excel file has been generated successfully.");
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
 }
